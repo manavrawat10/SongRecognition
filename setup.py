4f7c0c9f9ffdb881640a1952696be49d2cd6286d
Manvenddra Singh Rawat <manavrawat10@gmail.com>
2018-04-23 00:44:38 +0530

    Create setup.py
    Creating a setup file for making this as a package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='SongRecognition',
+    url='https://github.com/manavrawat10/SongRecognition',
+    author='Manvenddra Rawat',
+    author_email='manvenddra.rawat@gmail.com',
+    # Needed to actually package something
+    packages=['SongRecognition'],
+    # Needed for dependencies
+    install_requires=['SpeechRecognition','pyaudio','bs4'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='Maddy',
+    description='Song Recognition will listen to your song lyrics and will result in a song that you can play in youtube as well',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
