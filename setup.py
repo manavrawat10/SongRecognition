5a4529b01f557c0e46fb8a6fabab299156254a03
Manvenddra Singh Rawat <manavrawat10@gmail.com>
2018-04-24 00:41:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(
     # Needed for dependencies
     install_requires=['SpeechRecognition','pyaudio','bs4','requests','pywin32', 'lxml'],
     # *strongly* suggested for sharing
-    version='0.1',
+    version='0.3',
     # The license can be anything you like
-    license='Maddy',
+    license='Manvenddra',
     description='Song Recognition will listen to your song lyrics and will result in a song that you can play in youtube as well',
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
